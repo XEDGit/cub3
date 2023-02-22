b49e43c32d7d421a11774eab5c5acec7d235b697
sdxsd <willmaguire@icloud.com>
2023-02-22 13:10:18 +0100

    rendering.c largely converted.

--- src/main/rendering.c
+++ src/main/rendering.c
@@ -1,52 +1,57 @@
 #include "../../includes/rendering.h"
+#include <stdlib.h>
 
-static void	setupStepDirection(t_rayvars *ray, t_raycam *rayCam) {
+#define WIN_HEIGHT 1152
+#define WIN_WIDTH 1152
+
+static void	setup_step_direction(t_rayvars *ray, t_raycam *rayCam) {
 	if (ray->raydir.x < 0) {
 		ray->stepdirection.x = -1;
-		ray->sideDistances.x = (rayCam->campos.x - ray->int_map_coords.x) * ray->deltaDistances.x;
+		ray->sidedistances.x = (rayCam->campos.x - ray->int_map_coords.x) * ray->deltadistances.x;
 	} else {
 		ray->stepdirection.x = 1;
-		ray->sideDistances.x = (ray->int_map_coords.x + 1->0 - rayCam->campos.x) * ray->deltaDistances.x;
+		ray->sidedistances.x = (ray->int_map_coords.x + 1.0 - rayCam->campos.x) * ray->deltadistances.x;
 	}
-	if (ray->raydir->y < 0) {
-		ray->stepdirection->y = -1;
-		ray->sideDistances->y = (rayCam->campos->y - ray->int_map_coords->y) * ray->deltaDistances->y;
+	if (ray->raydir.y < 0) {
+		ray->stepdirection.y = -1;
+		ray->sidedistances.y = (rayCam->campos.y - ray->int_map_coords.y) * ray->deltadistances.y;
 	} else {
-		ray->stepdirection->y = 1;
-		ray->sideDistances->y = (ray->int_map_coords->y + 1->0 - rayCam->campos->y) * ray->deltaDistances->y;
+		ray->stepdirection.y = 1;
+		ray->sidedistances.y = (ray->int_map_coords.y + 1.0 - rayCam->campos.y) * ray->deltadistances.y;
 	}
 }
 
-static int castUntilHit(t_rayvars *ray, char **map) {
+// DDA Algorithm.
+static int cast_till_hit(t_rayvars *ray, char **map) {
 	int	hit = 0;
 	int	side;
 
 	while (hit == 0) {
-		if (ray.sideDistances.x < ray.sideDistances.y) {
-			ray.sideDistances.x += ray.deltaDistances.x;
-			ray.ImapCoords.x += ray.stepDirection.x;
+		if (ray->sidedistances.x < ray->sidedistances.y) {
+			ray->sidedistances.x += ray->deltadistances.x;
+			ray->int_map_coords.x += ray->stepdirection.x;
 			side = 0;
 		} else {
-			ray.sideDistances.y += ray.deltaDistances.y;
-			ray.ImapCoords.y += ray.stepDirection.y;
+			ray->sidedistances.y += ray->deltadistances.y;
+			ray->int_map_coords.y += ray->stepdirection.y;
 			side = 1;
 		}
-		if (map.getCoord(ray.ImapCoords.x, ray.ImapCoords.y) == '#') {
+		if (map[ray->int_map_coords.y][ray->int_map_coords.x] == 1) {
 			hit = 1;
 		}
 	}
 	return (side);
 }
 
-static VertLine genLine(RayVariables& ray, Map& map, int x, int side) {
-	VertLine	result;
+static t_vertline generate_line(t_rayvars *ray, char **map, int x, int side) {
+	t_vertline	result;
 	double		perpWallDist;
-	Color			color;
+	/* Color		color; */
 
 	if (side == 0)
-		perpWallDist = (ray.sideDistances.x - ray.deltaDistances.x);
+		perpWallDist = (ray->sidedistances.x - ray->deltadistances.x);
 	else
-		perpWallDist = (ray.sideDistances.y - ray.deltaDistances.y);
+		perpWallDist = (ray->sidedistances.y - ray->deltadistances.y);
 	int lineHeight = (int)((WIN_HEIGHT / perpWallDist));
 	int drawStart = -lineHeight / 2 + WIN_HEIGHT / 2;
 	if (drawStart < 0)
@@ -54,38 +59,39 @@ static VertLine genLine(RayVariables& ray, Map& map, int x, int side) {
 	int drawEnd = lineHeight / 2 + WIN_HEIGHT / 2;
 	if (drawEnd >= WIN_HEIGHT)
 		drawEnd = WIN_HEIGHT - 1;
-	color = getWallColor(map.getCoord(ray.ImapCoords.x, ray.ImapCoords.y));
-	if (side == 1) {
-		color.r -= 50, color.b -= 10, color.g -= 10;
-	};
-	result.xCoord = x;
-	result.startPoint = drawStart;
-	result.endPoint = drawEnd;
-	result.color = color;
+	/* color = getWallColor(map.getCoord(ray->int_map_coords.x, ray->int_map_coords.y)); */
+	/* if (side == 1) { */
+	/* 	color.r -= 50, color.b -= 10, color.g -= 10; */
+	/* }; */
+	result.xcoord = x;
+	result.startpoint = drawStart;
+	result.endpoint = drawEnd;
+	/* result.color = color; */
 	return (result);
 }
 
-VertLine RayCaster::castRay(RayCamera &rayCam, int x) {
-	RayVariables	ray;
-	int						side;
+t_vertline castRay(t_raycam *raycam, char **map, int x) {
+	t_rayvars	ray;
+	int			side;
 
-	ray.cameraX = 2 * x / double(WIN_WIDTH) - 1;
-	ray.rayDir.x = rayCam.dV.x + rayCam.pV.x * ray.cameraX;
-	ray.rayDir.y = rayCam.dV.y + rayCam.pV.y * ray.cameraX;
-	ray.ImapCoords = { int(rayCam.camPos.x), int(rayCam.camPos.y) };
-	ray.deltaDistances = { abs(1 / ray.rayDir.x), abs(1 / ray.rayDir.y) };
-	setupStepDirection(ray, rayCam);
-	side = castUntilHit(ray, map);
-	return (genLine(ray, map, x, side));
+	ray.camera_x = 2 * x / (double)WIN_WIDTH - 1;
+	ray.raydir.x = raycam->dv.x + raycam->pv.x * ray.camera_x;
+	ray.raydir.y = raycam->dv.y + raycam->pv.y * ray.camera_x;
+	ray.int_map_coords = (t_intvec2){ (int)raycam->campos.x, (int)raycam->campos.y };
+	ray.deltadistances = (t_vec2){ abs((int)(1 / ray.raydir.x)), abs((int)(1 / ray.raydir.y)) };
+	setup_step_direction(&ray, raycam);
+	side = cast_till_hit(&ray, map);
+	return (generate_line(&ray, map, x, side));
 }
 
-void	RayCaster::drawVert(VertLine line) {
-	ImageDrawLine(&imageBuffer, line.xCoord, line.startPoint, line.xCoord, line.endPoint, line.color);
-}
+/* void	drawVert(t_vertline line) { */
+/* 	ImageDrawLine(&imageBuffer, line.xCoord, line.startPoint, line.xCoord, line.endPoint, line.color); */
+/* } */
 
-Image& RayCaster::renderFrame() {
-	ImageClearBackground(&imageBuffer, BLACK);
+void renderFrame() {
+	/* ImageClearBackground(&imageBuffer, BLACK); */
 	for (int x = 0; x <= WIN_WIDTH; x++)
-		drawVert(castRay(rayCam, x));
-	return (imageBuffer);
+		;
+		/* drawVert(castRay(rayCam, x)); */
+	/* return (imageBuffer); */
 }
