400261f7a6ac0540eb1dfe698ecb6889df94587a
sdxsd <willmaguire@icloud.com>
2023-03-17 18:25:40 +0100

    Speed now based on frametime.

--- src/main/movement.c
+++ src/main/movement.c
@@ -14,10 +14,6 @@
 #include <stdio.h>
 #include "../../includes/rendering.h"
 
-/* Movement speed & rotation speed. */
-#define MOVS 0.05
-#define ROTS 0.05
-
 /* void	forward_backward(mlx_key_data_t k, t_raycam *rc, char **m, mlx_t *mlx) */
 /* { */
 /* } */
@@ -31,42 +27,46 @@ int	handle_input(t_raycam *rc, char **m, mlx_t *mlx)
 	int		has_moved;
 	double	old_dir_x;
 	double	old_plane_x;
+	double	movs;
+	double	rots;
 
+	movs = mlx->delta_time * 4.0;
+	rots = mlx->delta_time * 2.0;
 	has_moved = false;
 	if (mlx_is_key_down(mlx, MLX_KEY_UP))
 	{
-		if (m[(int)rc->campos.y][(int)(rc->campos.x + rc->dv.x * MOVS)] != '1')
-			rc->campos.x += rc->dv.x * MOVS;
-		if (m[(int)(rc->campos.y + rc->dv.y * MOVS)][(int)rc->campos.x] != '1')
-			rc->campos.y += rc->dv.y * MOVS;
+		if (m[(int)rc->campos.y][(int)(rc->campos.x + rc->dv.x * movs)] != '1')
+			rc->campos.x += rc->dv.x * movs;
+		if (m[(int)(rc->campos.y + rc->dv.y * movs)][(int)rc->campos.x] != '1')
+			rc->campos.y += rc->dv.y * movs;
 		has_moved = true;
 	}
 	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
 	{
-		if (m[(int)rc->campos.y][(int)(rc->campos.x - rc->dv.x * MOVS)] != '1')
-			rc->campos.x -= rc->dv.x * MOVS;
-		if (m[(int)(rc->campos.y - rc->dv.y * MOVS)][(int)rc->campos.x] != '1')
-			rc->campos.y -= rc->dv.y * MOVS;
+		if (m[(int)rc->campos.y][(int)(rc->campos.x - rc->dv.x * movs)] != '1')
+			rc->campos.x -= rc->dv.x * movs;
+		if (m[(int)(rc->campos.y - rc->dv.y * movs)][(int)rc->campos.x] != '1')
+			rc->campos.y -= rc->dv.y * movs;
 		has_moved = true;
 	}
 	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
 	{
 		old_dir_x = rc->dv.x;
-		rc->dv.x = rc->dv.x * cosf(-ROTS) - rc->dv.y * sinf(-ROTS);
-		rc->dv.y = old_dir_x * sinf(-ROTS) + rc->dv.y * cosf(-ROTS);
+		rc->dv.x = rc->dv.x * cosf(-rots) - rc->dv.y * sinf(-rots);
+		rc->dv.y = old_dir_x * sinf(-rots) + rc->dv.y * cosf(-rots);
 		old_plane_x = rc->pv.x;
-		rc->pv.x = rc->pv.x * cosf(-ROTS) - rc->pv.y * sinf(-ROTS);
-		rc->pv.y = old_plane_x * sinf(-ROTS) + rc->pv.y * cosf(-ROTS);
+		rc->pv.x = rc->pv.x * cosf(-rots) - rc->pv.y * sinf(-rots);
+		rc->pv.y = old_plane_x * sinf(-rots) + rc->pv.y * cosf(-rots);
 		has_moved = true;
 	}
 	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
 	{
 		old_dir_x = rc->dv.x;
-		rc->dv.x = rc->dv.x * cosf(ROTS) - rc->dv.y * sinf(ROTS);
-		rc->dv.y = old_dir_x * sinf(ROTS) + rc->dv.y * cosf(ROTS);
+		rc->dv.x = rc->dv.x * cosf(rots) - rc->dv.y * sinf(rots);
+		rc->dv.y = old_dir_x * sinf(rots) + rc->dv.y * cosf(rots);
 		old_plane_x = rc->pv.x;
-		rc->pv.x = rc->pv.x * cosf(ROTS) - rc->pv.y * sinf(ROTS);
-		rc->pv.y = old_plane_x * sinf(ROTS) + rc->pv.y * cosf(ROTS);
+		rc->pv.x = rc->pv.x * cosf(rots) - rc->pv.y * sinf(rots);
+		rc->pv.y = old_plane_x * sinf(rots) + rc->pv.y * cosf(rots);
 		has_moved = true;
 	}
 	printf("dv.x: %f | dv.y %f\n", rc->dv.x, rc->dv.y);
