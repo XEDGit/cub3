b7da48bdf5102b3b625254688fe3f7c0560bf2c4
sdxsd <willmaguire@icloud.com>
2023-03-17 14:15:54 +0100

    Really ugly refactor. Fuck norminette.

--- src/main/rendering.c
+++ src/main/rendering.c
@@ -72,6 +72,32 @@ static int	cast_till_hit(t_rayvars *ray, char **map)
 	return (side);
 }
 
+static void	tex_coords(t_renderer *rn, int side, int x, t_vertline *r)
+{
+	double	wall_x;
+	double	perpwalldist;
+	int		lineheight;
+
+	if (side == 0)
+		perpwalldist = (rn->rv->sidedistances.x - rn->rv->deltads.x);
+	else
+		perpwalldist = (rn->rv->sidedistances.y - rn->rv->deltads.y);
+	lineheight = ((H / perpwalldist));
+	if (side == 0)
+		wall_x = rn->rc->campos.y + perpwalldist * rn->rv->rdir.y;
+	else
+		wall_x = rn->rc->campos.x + perpwalldist * rn->rv->rdir.x;
+	wall_x -= floor((wall_x));
+	r->tex_x = (int)(wall_x * (double)TW);
+	if (side == 0 && rn->rv->rdir.x > 0)
+		r->tex_x = TW - r->tex_x - 1;
+	if (side == 1 && rn->rv->rdir.y < 0)
+		r->tex_x = TW - r->tex_x - 1;
+	r->step = 1.0 * TH / lineheight;
+	r->tex_y_begin_pos = (r->startpoint - (H / 2) + (lineheight / 2)) * r->step;
+	r->side = side;
+}
+
 static t_vertline	generate_line(t_renderer *rn, char **map, int x, int side)
 {
 	t_vertline	r;
@@ -91,19 +117,6 @@ static t_vertline	generate_line(t_renderer *rn, char **map, int x, int side)
 	if (r.endpoint >= H)
 		r.endpoint = H - 1;
 	r.xcoord = x;
-	if (side == 0)
-		wall_x = rn->rc->campos.y + perpwalldist * rn->rv->rdir.y;
-	else
-		wall_x = rn->rc->campos.x + perpwalldist * rn->rv->rdir.x;
-	wall_x -= floor((wall_x));
-	r.tex_x = (int)(wall_x * (double)TW);
-	if (side == 0 && rn->rv->rdir.x > 0)
-		r.tex_x = TW - r.tex_x - 1;
-	if (side == 1 && rn->rv->rdir.y < 0)
-		r.tex_x = TW - r.tex_x - 1;
-	r.step = 1.0 * TH / lineheight;
-	r.tex_y_begin_pos = (r.startpoint - (H / 2) + (lineheight / 2)) * r.step;
-	r.side = side;
 	return (r);
 }
 
